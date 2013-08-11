c86233c31cfd53b3113a92aeb9e12a0a36df9e18
rbas <rbas.cz@gmail.com>
2013-08-11 14:25:34 +0200

    Change path in setup script

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-
-
 import os
 import sys
 import shlex
@@ -11,16 +9,6 @@ try:
     from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup, find_packages  # NOQA
-
-# Hack to prevent stupid "TypeError: 'NoneType' object is not callable"
-# error in multiprocessing/util.py _exit_function when running `python
-# setup.py test`
-# try:
-#     import multiprocessing  # NOQA
-# except ImportError:
-#     pass
-
-base_path = os.path.dirname(__file__)
 
 version = django_fiobank.__versionstr__
 
@@ -38,12 +26,12 @@ setup(
     version=version,
     description=('Django-app for managing and processing Fio Bank '
                  'transaction'),
-    long_description=open(os.path.join(base_path, 'README.rst')).read(),
+    long_description=open('README.rst').read(),
     author='Martin Voldrich',
     author_email='rbas.cz@gmail.com',
     url='https://github.com/rbas/django-fiobank',
     packages=find_packages(),
-    license=open(os.path.join(base_path, 'LICENSE')).read(),
+    license=open('LICENSE').read(),
     install_requires=['fiobank>=0.0.3,<0.1', 'south'],
     classifiers=(
         'Development Status :: 4 - Beta',
