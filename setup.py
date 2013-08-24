b4b70d5f68ce02cb51e11172cb6c3ea10eb46ecb
rbas <rbas.cz@gmail.com>
2013-08-24 15:55:08 +0200

    Upgrade fiobank library

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     url='https://github.com/rbas/django-fiobank',
     packages=find_packages(),
     license=read_file('LICENSE'),
-    install_requires=['fiobank>=0.0.3,<0.1', 'south'],
+    install_requires=['fiobank>=0.0.4,<0.1', 'south'],
     include_package_data=True,
     package_data={'django_fiobank': ['locale/*/LC_MESSAGES/*']},
     cmdclass={'build': Build, 'install_lib': InstallLib,
